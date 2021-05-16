045752121c24e76954dcd05be389b3023d565b20
jjart27 <jjart@mubbus.us>
2021-05-15 20:31:38 -0500

    continued massaging

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Input, Button } from '@material-ui/core';
+import { TextField, Button } from '@material-ui/core';
 import { newsSearch } from '../actions';
 import _ from 'lodash';
-
 
 class Search extends Component {
 	constructor(props) {
@@ -21,20 +20,33 @@ class Search extends Component {
 		})
 	};
 
-	searchHandler = event => {
-		this.props.newsSearch(this.state.value)
+	submitHandler = event => {
+		console.log(this.state.value, " / ", this.state.value.length)
+		if (this.state.value.length > 2) {			
+			this.props.newsSearch(this.state.value)
+
+			this.setState({
+				textFieldMessage: null
+			})
+		} else {
+			this.setState({
+				textFieldMessage: "Please enter a valid search term."
+			})
+		}
 	}
 
 	render() {
 		const { props } = this.props
+
 		// console.log("search render")
 		// console.log("state: ", this.state)
 		console.log("props: ", this.props)
+		console.log("state: ", this.state)
 
 		const searchResults = _.get(this.props,'searchResults', []);
 		let resultsCount;
 		if (searchResults.length > 0) {
-			resultsCount = <div>{this.props.searchResults.length} results found</div>;
+			resultsCount = <div className="resultsCount"><span>{this.props.searchResults.length} results returned</span></div>;
 		}
 
 		const searchMessage = _.get(this.props,'searchMessage');
@@ -46,33 +58,42 @@ class Search extends Component {
 		return (
 			<div>
 
-				<div>search</div>
+				<h1>search</h1>
+				<p>
+					Use the search field below to search the Hacker News Algolia API and displays a list of results.
+				</p>
 
 					<div>
-					<Input
+					<TextField
 						multiline={false}
 						onChange={this.handleChange}
 						value={this.state.value}
+						variant="outlined" 
+						placeholder="Search" 
+						required
 					/>
-					</div>
 
-					<div>
-					<Button variant="outlined"  aria-label="Search"  onClick={this.searchHandler} >
+					<Button variant="outlined"  aria-label="Search"  onClick={this.submitHandler} style={{ margin:"0 0 0 15px", padding: "6px 20px" }} >
 						Search
 					</Button>
+
+					<span style={{ color:"red", paddingLeft:"20px" }}>{this.state.textFieldMessage}</span>
+
 					</div>
 
-					<hr />
 					
 					{resultsMessage}
 					{resultsCount}
 
+					<div className="searchResultsList">
 					{searchResults.map(function(r, index){
 						return <div key={ index }>{r.title}</div>;
 					})}
+					</div>
 
 					<hr />
 
+					<span >view raw data</span>
 					<pre className="codeBlock">
 						{JSON.stringify(this.props.searchResults, null, 2) }
 					</pre>
